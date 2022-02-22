7a3192e8ba613b0a7d03f855237b257b275dfac2
dean <dnabusood@gmail.com>
2022-02-21 21:49:00 -0500

    Update styles and simplify functions

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -7,12 +7,11 @@ function createPad(size = 16){
     for(let i = 0; i < totalDivs; i++){
         let div = document.createElement("div");
         div.classList.add("inner-pad");
-        div.setAttribute("style", "background-color: white");
+        div.setAttribute("style", "background-color: white;");
 
         div.addEventListener('mouseover', () => {
-            div.style.backgroundColor = "black";
+            div.style.backgroundColor = "darkolivegreen";
         });
-
 
         pad.appendChild(div);
     }
@@ -21,8 +20,8 @@ function createPad(size = 16){
 function changePadSize(size){
     if(size >= 2 && size <= 100){
     const padDivs = document.querySelectorAll(".inner-pad");
-        padDivs.forEach(pad => {
-        pad.remove();
+        padDivs.forEach(div => {
+        div.remove();
     });
     createPad(size);
     }  
@@ -32,17 +31,56 @@ function changePadSize(size){
     }
 }
 
-function clearPad(){
+//applies function for each button clicked
+function buttonClicked(button){
+    if(button == "color-button"){
+        triggerColor();
+    }
+    else if(button == "eraser-button"){
+        triggerEraser();
+    }
+    else if(button == "clear-button"){
+        clearPad();
+    }
+}
+
+// event listener added on every button
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button =>{
+    button.addEventListener('click', () =>{
+        buttonClicked(button.id);
+    });
+});
+
+// controls black button
+function triggerColor(){
     const padDivs = document.querySelectorAll(".inner-pad");
-        padDivs.forEach(pad => {
-        pad.style.backgroundColor = "white";
+        padDivs.forEach(div => {
+        div.addEventListener("mouseover", () =>{
+            div.style.backgroundColor = "darkolivegreen";
+        });
     });
 }
+
+// controls eraser button
+function triggerEraser(){
+    const padDivs = document.querySelectorAll(".inner-pad");
+        padDivs.forEach(div => {
+        div.addEventListener("mouseover", () =>{
+            div.style.backgroundColor = "white";
+        });
+    });
+}
+
+// controls clear button
+function clearPad(){
+    const padDivs = document.querySelectorAll(".inner-pad");
+    padDivs.forEach(div => {
+    div.style.backgroundColor = "white";
+});   
+}
+
 
 
 createPad();
 
-const clearButton = document.querySelector("#clear");
-clearButton.addEventListener('click', () =>{
-    clearPad();
-});
