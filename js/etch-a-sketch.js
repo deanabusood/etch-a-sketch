44682ebf4b071536f88c8625efb26d0c3afef084
dean <dnabusood@gmail.com>
2022-02-21 20:25:26 -0500

    Add functions to create and clear pad

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,3 +1,48 @@
-const container = document.querySelector("#container");
 
-// function createElements
+function createPad(size = 16){
+    const pad = document.querySelector(".pad");
+    let totalDivs = size * size;
+    let width = 960 / size;
+
+    for(let i = 0; i < totalDivs; i++){
+        let div = document.createElement("div");
+        div.classList.add("inner-pad");
+        div.setAttribute("style", "background-color: white");
+
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = "black";
+        });
+
+
+        pad.appendChild(div);
+    }
+}
+
+function changePadSize(size){
+    if(size >= 2 && size <= 100){
+    const padDivs = document.querySelectorAll(".inner-pad");
+        padDivs.forEach(pad => {
+        pad.remove();
+    });
+    createPad(size);
+    }  
+    else{
+        //alert or do trigger something
+        return;
+    }
+}
+
+function clearPad(){
+    const padDivs = document.querySelectorAll(".inner-pad");
+        padDivs.forEach(pad => {
+        pad.style.backgroundColor = "white";
+    });
+}
+
+
+createPad();
+
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener('click', () =>{
+    clearPad();
+});
